bf11012e04226ae8acea6468496608e67e1a011f
Davut <sergeev.davutt@gmail.com>
2026-04-30 06:05:27 +0400

    frontend: landing page

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware('guest.token');
 
 Route::get('/{any}', function () {
     return view('app');
